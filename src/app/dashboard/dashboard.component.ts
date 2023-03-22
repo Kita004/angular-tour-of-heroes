dac7c98815abb32e60dc7e3c92ace060fe4e3bc8
Kitti Born <born.kitti97@gmail.com>
2023-03-22 10:36:45 +0100

    refactor routing so that dashboard is default page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../services/heroService/hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
